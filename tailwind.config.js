c537bcb7708f6fb32126ea7a277c5fd7ee53ac7b
enoquetembe <enoquetembetembe10@gmail.com>
2023-04-30 13:53:39 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+
+  theme: {
+    extend: {
+      colors: {
+        "timer-green-dark": "#015F43",
+        "timer-green": "#00875F",
+        "timer-green-light": "#00B37E",
+
+        "timer-red-dark": "#7A1921",
+        "timer-red-": "#F03847",
+
+        "timer-gray-900": "#121214",
+        "timer-gray-800": "#202024",
+        "timer-gray-700": "#29292E",
+        "timer-gray-600": "##323238",
+        "timer-gray-500": "#7C7C8A",
+        "timer-gray-400": "#8D8D99",
+        "timer-gray-300": "#C4C4CC",
+        "timer-gray-100": "#E1E1E6",
+
+        "timer-yellow": "#FBA94C"
+      },
+
+      fontFamily: {
+        roboto: "'Roboto', sans-serif",
+        "roboto-mono": "'Roboto Mono', monospace",
+      },
+    },
+  },
+  plugins: [],
+};
