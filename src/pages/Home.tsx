6f5a733d749328c5cc7f64e7ec73edeca86aabce
enoquetembe <enoquetembetembe10@gmail.com>
2023-05-06 10:30:33 -0300

    validating form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,45 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Task is required'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
+type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  function handleCreateNewCycle(data: newCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
   return (
     <div className="flex-1 flex flex-col justify-center items-center">
-      <form className="flex flex-col items-center gap-14">
+      <form
+        onSubmit={handleSubmit(handleCreateNewCycle)}
+        className="flex flex-col items-center gap-14"
+      >
         <div className="w-full flex items-center justify-center gap-2 flex-wrap text-timer-gray-100 text-lg font-bold">
           <label htmlFor="task">I will work on</label>
           <input
             type="text"
             id="task"
             list="task-suggestion"
+            {...register('task')}
             placeholder="Name your project"
             className="bg-transparent h-9 p-2 border-b-2 font-bold text-lg border-b-timer-gray-500 flex-1 placeholder:text-timer-gray-500 focus:shadow-none focus:border-b-timer-green"
           />
@@ -27,6 +57,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
             placeholder="00"
             className="bg-transparent w-16 h-9 p-2 border-b-2 font-bold text-lg border-b-timer-gray-500 placeholder:text-timer-gray-500 focus:shadow-none focus:border-b-timer-green"
           />
@@ -46,7 +77,8 @@ export function Home() {
 
         <button
           type="submit"
-          className="bg-timer-green text-timer-gray-100 w-full rounded-lg flex justify-center items-center gap-2 p-4 font-bold hover:bg-timer-green-dark disabled:opacity-70 disabled:hover:bg-timer-green"
+          disabled={isSubmitDisabled}
+          className="bg-timer-green text-timer-gray-100 w-full rounded-lg flex justify-center items-center gap-2 p-4 font-bold hover:bg-timer-green-dark disabled:opacity-70 disabled:hover:bg-timer-green disabled:cursor-not-allowed"
         >
           <Play size={24} />
           Start
