8c8ff356c106d2f7ca84572dcc2a8d3c74629f5e
enoquetembe <enoquetembetembe10@gmail.com>
2023-05-08 19:45:43 -0300

    renaming Router file to solve netlify deploy bug

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { CyclesContextProvider } from './contexts/CyclesContext.tsx'
-import { AppRouter } from './routes/Router.tsx'
+import { AppRouter } from './routes/AppRouter.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
