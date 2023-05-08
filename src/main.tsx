2368709df562c76c6a42a4cacb7e69c7f77e9cea
enoquetembe <enoquetembetembe10@gmail.com>
2023-05-08 19:30:10 -0300

    solving netlify bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './styles/global.css'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Router } from './routes/Router.tsx'
+
 import { CyclesContextProvider } from './contexts/CyclesContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
